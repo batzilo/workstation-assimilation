bb08431107471c5293766848de712d2ee3c89cc5
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-12-16 21:54:33 +0200

    Only enforce MFA if user has set it up

--- cookbooks/assimilation/recipes/sshd_mfa_google.rb
+++ cookbooks/assimilation/recipes/sshd_mfa_google.rb
@@ -6,6 +6,18 @@ package %w(
   action :upgrade
 end
 
+ruby_block 'Set AuthenticationMethods to both publickey and keyboard-interactive' do
+  block do
+    line = 'AuthenticationMethods'
+    file = Chef::Util::FileEdit.new('/etc/ssh/sshd_config')
+    file.search_file_replace(/^#{line}.*$/, "#{line} publickey,keyboard-interactive")
+    file.write_file
+  end
+  # Do nothing unless user has run `google-authenticator` for setup.
+  only_if { File.exists?('/home/batzilo/.google_authenticator') }
+end
+
+# Commented-out, see next section.
 # execute 'Disable password authentication' do
 #   command 'sed -i s/^@include common-auth$/#@include common-auth/ /etc/pam.d/sshd'
 # end
@@ -17,8 +29,11 @@ ruby_block 'Disable password authentication' do
     file.search_file_replace(/^#{line}$/, "# vsoul\n##{line}")
     file.write_file
   end
+  # Do nothing unless user has run `google-authenticator` for setup.
+  only_if { File.exists?('/home/batzilo/.google_authenticator') }
 end
 
+# Commented-out, see next section.
 # execute 'Enable the Google Authenticator PAM module' do
 #   command 'echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd'
 #   not_if 'grep -q "auth required pam_google_authenticator.so" /etc/pam.d/sshd'
@@ -31,6 +46,8 @@ ruby_block 'Enable the Google Authenticator PAM module' do
     file.insert_line_if_no_match(/#{line}/, "\n# vsoul\n#{line}")
     file.write_file
   end
+  # Do nothing unless user has run `google-authenticator` for setup.
+  only_if { File.exists?('/home/batzilo/.google_authenticator') }
 end
 
 execute 'Restart the sshd service' do
