2ac573e690d11556e255747b9a9e9d6662641db2
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-12-30 23:41:06 +0200

    Install python3-certbot-nging on Zion

--- cookbooks/assimilation/recipes/nginx.rb
+++ cookbooks/assimilation/recipes/nginx.rb
@@ -5,3 +5,23 @@ package %w(
 ) do
   action :upgrade
 end
+
+# Install the Python 3 dependencies for the `python3-certbot-nginx` package.
+package %w(
+  python3-acme
+  python3-certbot
+  python3-mock
+  python3-openssl
+  python3-pkg-resources
+  python3-pyparsing
+  python3-zope.interface
+) do
+  action :upgrade
+end
+
+# Install the `python3-certbot-nginx` package.
+package %w(
+  python3-certbot-nginx
+) do
+  action :upgrade
+end
