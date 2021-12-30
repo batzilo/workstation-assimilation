e674fc1590b2b1e2a9d48b924442c6c02dac008f
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-12-31 00:15:05 +0200

    Add the zion.vsoul.net nginx site on Zion

--- cookbooks/assimilation/recipes/nginx.rb
+++ cookbooks/assimilation/recipes/nginx.rb
@@ -6,6 +6,22 @@ package %w(
   action :upgrade
 end
 
+template '/etc/nginx/sites-available/zion.vsoul.net.conf' do
+  source 'etc_nginx_sites-available_zion.vsoul.net.conf.erb'
+  mode 644
+  not_if { ::File.exist?('/etc/nginx/sites-available/zion.vsoul.net.conf') }
+end
+
+execute 'Disable the default nginx site' do
+  command 'rm /etc/nginx/sites-enabled/default'
+  only_if { ::File.exist?('/etc/nginx/sites-enabled/default') }
+end
+
+execute 'Enable the zion.vsoul.net nginx site' do
+  command 'ln -s /etc/nginx/sites-{available,enabled}/zion.vsoul.net.conf'
+  not_if { ::File.exist?('/etc/nginx/sites-available/zion.vsoul.net.conf') }
+end
+
 # Install the Python 3 dependencies for the `python3-certbot-nginx` package.
 package %w(
   python3-acme
@@ -25,3 +41,5 @@ package %w(
 ) do
   action :upgrade
 end
+
+# You can now run `# certbot --nginx` to obtain SSL certificates.
