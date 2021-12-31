3115e115b13a04e57c1e1c98ee8b7f868e33bad7
Vasileios Souleles <vasilis.souleles@gmail.com>
2021-12-31 12:13:56 +0200

    Install docker compose on Zion

--- cookbooks/assimilation/recipes/docker.rb
+++ cookbooks/assimilation/recipes/docker.rb
@@ -14,6 +14,7 @@ execute 'docker_apt_update' do
   command 'apt update'
 end
 
+# Install Docker.
 package %w(
   docker-ce
   docker-ce-cli
@@ -22,6 +23,7 @@ package %w(
   action :upgrade
 end
 
+# Add users to the docker group.
 %w[batzilo].each do |user|
   # Add user to docker group
   group "docker_add_#{user}" do
@@ -30,5 +32,39 @@ end
     members user
     action :manage
     only_if "getent passwd #{user}"
+  end
+end
+
+# Install docker compose.
+%w[batzilo].each do |user|
+  # Create the `~/.docker` directory.
+  execute 'mkdir -p ~/.docker/cli-plugins/' do
+    command "mkdir -p /home/#{user}/.docker/cli-plugins/"
+    user user
+    not_if { ::File.exist?("/home/#{user}/.docker/cli-plugins/docker-compose") }
+  end
+
+  # Download the `docker compose` binary from GitHub.
+  execute 'Download the docker compose binary from GitHub' do
+    command "curl -SL https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o /home/#{user}/.docker/cli-plugins/docker-compose"
+    user user
+    not_if { ::File.exist?("/home/#{user}/.docker/cli-plugins/docker-compose") }
+  end
+
+  # Make the `docker compose` binary executable.
+  execute 'Apply executable permissions to the docker compose binary' do
+    command "chmod +x /home/#{user}/.docker/cli-plugins/docker-compose"
+    user user
+  end
+
+  # Check if `docker compose` was installed successfully.
+  execute 'Check the docker compose version' do
+    command 'docker compose version'
+    returns 0
+    user user
+    environment ({
+      'HOME' => "/home/#{user}",
+      'USER' => user,
+    })
   end
 end
