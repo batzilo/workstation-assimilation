b9b7ad1a0d8d10299c3cf5c6887c31a2f4063a09
Vasileios Souleles <vasilis.souleles@gmail.com>
2022-01-02 01:27:46 +0200

    Add all the vsoul.* domain names

--- cookbooks/assimilation/recipes/vsoul_net.rb
+++ cookbooks/assimilation/recipes/vsoul_net.rb
@@ -3,6 +3,6 @@
 # Create the vsoul.net static website.
 website 'vsoul.net' do
   domain_name 'vsoul.net'
-  domain_names 'vsoul.net www.vsoul.net'
+  domain_names 'vsoul.net www.vsoul.net vsoul.tech www.vsoul.tech vsoul.info www.vsoul.info vsoul.gr www.vsoul.gr batzilo.gr www.batzilo.gr'
   action :create
 end
