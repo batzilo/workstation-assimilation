4232c17ce7eb0545dbf44695fa1bfea942262ce7
Vasileios Souleles <vasilis.souleles@gmail.com>
2024-06-28 00:48:57 +0300

    Configure Kurt keyboard backlight timeout in sysfs

--- cookbooks/assimilation/recipes/kurt_backlight_udev.rb
+++ cookbooks/assimilation/recipes/kurt_backlight_udev.rb
@@ -5,6 +5,11 @@ template '/etc/udev/rules.d/99-intel-backlight.rules' do
   mode 644
 end
 
+template '/etc/udev/rules.d/99-dell_kbd_backlight.rules' do
+  source 'etc_udev_rulesd_99dellkbdbacklight.erb'
+  mode 644
+end
+
 execute 'Reload udev rules' do
   command 'udevadm control --reload'
 end
